9c4cf1e013f5ed1c57f0b1ad25aa05984d24c0ae
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-02-07 18:23:04 +0300

    add brands section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Brands from "./components/brands/Brands";
 import Header from "./components/header/Header";
 import Promo from "./components/promo/Promo";
 
@@ -5,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Promo />
+      {/* <Promo /> */}
+      <Brands />
     </div>
   );
 }
